11cf217c9d586e37af070645c72bebe566d65e66
tatsuya-maruyama <maruyama@wavesync.co.jp>
2019-12-17 15:43:35 +0900

    対象テーブル変更

--- api/getemployee.php
+++ api/getemployee.php
@@ -3,7 +3,7 @@ require '../header.php';
 
 $postdata = file_get_contents("php://input");
 
-$emps = ORM::for_table("tblemployee")->find_array();
+$emps = ORM::for_table(TBLUSER)->find_array();
 echo json_encode($emps);
 
 
