036b81b9e0390b19cba263e26d688395e696bfe1
tatsuya-maruyama <maruyama@wavesync.co.jp>
2019-12-17 18:03:41 +0900

    notCodeを追加

--- api/getcode.php
+++ api/getcode.php
@@ -4,12 +4,17 @@ require '../header.php';
 $postparam = file_get_contents("php://input");
 $param = json_decode($postparam);
 
+$query = ORM::for_table("tblcode");
+
 if(isset($param->code)){
-	$codes = ORM::for_table("tblcode")->where_in('code', $param->code)->find_array();
+	$codes = $codes->where_in('code', $param->code);
 }
-else {
-	$codes = ORM::for_table("tblcode")->find_array();
+
+if(isset($param->notCode)){
+	$codes = $codes->where_not_in('code', $param->notCode);
 }
+
+$codes = $codes->find_array();
 echo json_encode($codes);
 
 ?>
