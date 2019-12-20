9c553c0bc049b6316281426a7f7d152e3939216c
tatsuya-maruyama <maruyama@wavesync.co.jp>
2019-12-20 10:36:03 +0900

    where_null('deleteDate')

--- api/getdep.php
+++ api/getdep.php
@@ -3,8 +3,7 @@ require '../header.php';
 
 $postdata = file_get_contents("php://input");
 
-$deps = ORM::for_table("tbldepartment")->find_array();
+$deps = ORM::for_table("tbldepartment")->where_null('deleteDate')->find_array();
 echo json_encode($deps);
-
 
 ?>
