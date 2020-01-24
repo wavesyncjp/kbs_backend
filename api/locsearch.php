3db872d0e437d61265d7e99cbaeb9dbe0a4ac794
hirano-244 <hirano@wavesync.co.jp>
2020-01-24 17:56:47 +0900

    20200124bk_new

--- api/locsearch.php
+++ api/locsearch.php
@@ -4,11 +4,17 @@ require '../header.php';
 
 $postdata = file_get_contents("php://input");
 $param = json_decode($postdata);
-                     
+
 $query = ORM::for_table(TBLLOCATIONINFO)->where_null('deleteDate');
 
+if(isset($param->tempLandInfoPid) && $param->tempLandInfoPid !== ''){
+	$query = $query->where('tempLandInfoPid', $param->tempLandInfoPid);
+}
+if(isset($param->locationType) && $param->locationType !== ''){
+	$query = $query->where('locationType', $param->locationType);
+}
 
-$deps = $query->find_array();
-echo json_encode($deps);
+$locs = $query->find_array();
+echo json_encode($locs);
 
 ?>
