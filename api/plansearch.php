6e7df1ad24d3830c4218cd02f9ae6f4f8a751690
CWThun <tuan@wavesync.co.jp>
2020-04-07 16:30:47 +0900

    計画検索

--- api/plansearch.php
+++ api/plansearch.php
@@ -5,30 +5,42 @@ require '../header.php';
 $postdata = file_get_contents("php://input");
 $param = json_decode($postdata);
 
-$query = ORM::for_table(TBLPLAN)
+$query = ORM::for_table(TBLTEMPLANDINFO)
 			->table_alias('p1')
-			->select('p1.*')
-			->select('p2.bukkenNo', 'bukkenNo')
-			->select('p2.bukkenName', 'bukkenName');
-
-$query = $query->inner_join(TBLTEMPLANDINFO, array('p1.tempLandInfoPid', '=', 'p2.pid'), 'p2');
+			->distinct()
+            ->select('p1.*')
+            ->left_outer_join(TBLPLAN, array('p1.pid', '=', 'p2.tempLandInfoPid'), 'p2');
 
 $query = $query->where_null('p1.deleteDate');
 
 if(isset($param->bukkenNo) && $param->bukkenNo !== ''){
-	$query = $query->where_like('p2.bukkenNo', $param->bukkenNo.'%');
+	$query = $query->where_like('p1.bukkenNo', $param->bukkenNo.'%');
 }
 if(isset($param->bukkenName) && $param->bukkenName !== ''){
-	$query = $query->where_like('p2.bukkenName', '%'.$param->bukkenName.'%');
+	$query = $query->where_like('p1.bukkenName', '%'.$param->bukkenName.'%');
 }
 if(isset($param->address) && $param->address !== ''){
-	$query = $query->where_like('p2.address', '%'.$param->address.'%');
+	$query = $query->where_like('p1.address', '%'.$param->address.'%');
 }
 if(isset($param->planName) && $param->planName !== ''){
-	$query = $query->where_like('p1.planName', '%'.$param->planName.'%');
+	$query = $query->where_like('p2.planName', '%'.$param->planName.'%');
 }
 
-$ret = $query->find_array();
+$lands = $query->find_array();
+$ret = array();
+foreach($lands as $land){
+	$plans = ORM::for_table(TBLPLAN)->where('tempLandInfoPid', $land['pid'])
+			->where_null('deleteDate')
+			->select('pid')
+			->select('tempLandInfoPid')
+			->select('planName')
+			->select('createDate')
+			->select('updateDate')->find_array();
+	$obj = array('tempLandInfoPid' => $land['pid'], 'bukkenNo' => $land['bukkenNo'],'bukkenName' => $land['bukkenName'],'address' => '');
+	$obj['plans'] = $plans;
+	$ret[] = $obj;
+}
+
 echo json_encode($ret);
 
 ?>
