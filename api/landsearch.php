c1e30c16939518372b2d4f51b507ced3de6de5d9
tatsuya-maruyama <maruyama@wavesync.co.jp>
2020-05-19 08:17:46 +0900

    20200519_maruyama

--- api/landsearch.php
+++ api/landsearch.php
@@ -21,22 +21,34 @@ if(isset($param->address) && $param->address != ''){
 }						
 $query = $query->where_null('p1.deleteDate');
 
+// 物件番号
 if(isset($param->bukkenNo) && $param->bukkenNo !== ''){
 	$query = $query->where('p1.bukkenNo', $param->bukkenNo);
 }
+// 契約物件番号
+if(isset($param->contractBukkenNo) && $param->contractBukkenNo !== ''){
+	$query = $query->where('p1.contractBukkenNo', $param->contractBukkenNo);
+}
+// 物件名
 if(isset($param->bukkenName) && $param->bukkenName !== ''){
 	$query = $query->where_like('p1.bukkenName', '%'.$param->bukkenName.'%');
 }
+// 物件担当部署
 if(isset($param->department) && sizeof($param->department) > 0){
 	$query = $query->where_in('p1.department', $param->department);
 }
+// 結果
 if(isset($param->result) && sizeof($param->result) > 0){
 	$query = $query->where_in('p1.result', $param->result);
 }
+// 居住表示
+if(isset($param->residence) && $param->residence !== ''){
+	$query = $query->where_like('p1.residence', '%'.$param->residence.'%');
+}
+// 所在地
 if(isset($param->address) && $param->address != ''){
 	$query = $query->where_like('p2.address', '%'.$param->address.'%');
 }
-
 //情報収集日(pickDate)
 if(isset($param->pickDateSearch_From) && $param->pickDateSearch_From != ''){
 	$query = $query->where_gte('p1.pickDate', $param->pickDateSearch_From);
@@ -44,7 +56,6 @@ if(isset($param->pickDateSearch_From) && $param->pickDateSearch_From != ''){
 if(isset($param->pickDateSearch_To) && $param->pickDateSearch_To != ''){
 	$query = $query->where_lte('p1.pickDate', $param->pickDateSearch_To);
 }
-
 //測量依頼日(surveyRequestedDay)
 if(isset($param->surveyRequestedDaySearch_From) && $param->surveyRequestedDaySearch_From != ''){
 	$query = $query->where_gte('p1.surveyRequestedDay', $param->surveyRequestedDaySearch_From);
