bd129635572573b0231f6c0959ef90b733435cb9
tatsuya-maruyama <maruyama@wavesync.co.jp>
2020-05-26 23:26:49 +0900

    20200526_maruyama

--- api/plansearch.php
+++ api/plansearch.php
@@ -13,27 +13,38 @@ $query = ORM::for_table(TBLTEMPLANDINFO)
 
 $query = $query->where_null('p1.deleteDate');
 
+// 物件番号
 if(isset($param->bukkenNo) && $param->bukkenNo !== ''){
-	$query = $query->where_like('p1.bukkenNo', $param->bukkenNo.'%');
+	$query = $query->where_like('p1.bukkenNo', $param->bukkenNo);
 }
+// 物件名
 if(isset($param->bukkenName) && $param->bukkenName !== ''){
 	$query = $query->where_like('p1.bukkenName', '%'.$param->bukkenName.'%');
 }
+// 所在地
 if(isset($param->address) && $param->address !== ''){
-	$query = $query->where_like('p1.address', '%'.$param->address.'%');
+	$query = $query->where_like('p2.address', '%'.$param->address.'%');
 }
+// プラン名
 if(isset($param->planName) && $param->planName !== ''){
 	$query = $query->where_like('p2.planName', '%'.$param->planName.'%');
 }
+// 作成日
+if(isset($param->createDay) && $param->createDay != ''){
+	$query = $query->where('p2.createDay', $param->createDay);
+}
 
-$lands = $query->find_array();
+$lands = $query->order_by_desc('pid')->find_array();
 $ret = array();
+
 foreach($lands as $land){
 	$plans = ORM::for_table(TBLPLAN)->where('tempLandInfoPid', $land['pid'])
 			->where_null('deleteDate')
 			->select('pid')
 			->select('tempLandInfoPid')
+			->select('address')
 			->select('planName')
+			->select('createDay')
 			->select('createDate')
 			->select('updateDate')->find_array();
 	$obj = array('tempLandInfoPid' => $land['pid'], 'bukkenNo' => $land['bukkenNo'],'bukkenName' => $land['bukkenName'],'address' => '');
