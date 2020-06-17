b6e2f965a8ffe661601c4d8268b776d2cf1c3e65
CWThun <tuan@wavesync.co.jp>
2020-06-17 17:37:15 +0900

    物件検索修正

--- api/getland.php
+++ api/getland.php
@@ -2,8 +2,14 @@
 require '../header.php';
 
 $postdata = file_get_contents("php://input");
+$param = json_decode($postdata);
 
-$lands = ORM::for_table("tbltemplandinfo")->where_null('deleteDate')->find_array();
+if(isset($param->bukkenName) && $param->bukkenName !== '') {
+    $lands = ORM::for_table("tbltemplandinfo")->where_like("bukkenName", "%" . $param->bukkenName . "%")->where_null('deleteDate')->find_array();
+}
+else {
+    $lands = ORM::for_table("tbltemplandinfo")->where_null('deleteDate')->find_array();
+}
 echo json_encode($lands);
 
 ?>
