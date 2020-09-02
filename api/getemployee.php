8351196db2cf67c046e51e83afdc237cf1ad243b
tatsuya-maruyama <maruyama@wavesync.co.jp>
2020-09-02 17:25:56 +0900

    20200902_maruyama

--- api/getemployee.php
+++ api/getemployee.php
@@ -3,8 +3,7 @@ require '../header.php';
 
 $postdata = file_get_contents("php://input");
 
-$emps = ORM::for_table(TBLUSER)->find_array();
+$emps = ORM::for_table(TBLUSER)->where_null('deleteDate')->find_array();
 echo json_encode($emps);
-
 
 ?>
