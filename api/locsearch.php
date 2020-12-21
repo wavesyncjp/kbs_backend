00f2efd2076cfb1e1ba54282f26d11f91727bc85
tatsuya-maruyama <maruyama@wavesync.co.jp>
2020-12-21 12:34:48 +0900

    20201221_maruyama

--- api/locsearch.php
+++ api/locsearch.php
@@ -13,6 +13,12 @@ if(isset($param->tempLandInfoPid) && $param->tempLandInfoPid !== ''){
 if(isset($param->locationType) && $param->locationType !== ''){
 	$query = $query->where('locationType', $param->locationType);
 }
+// 20201221 S_Add
+// clct結果
+if(isset($param->clctLocationType) && $param->clctLocationType != ''){
+	$query = $query->where_in('locationType', $param->clctLocationType);
+}
+// 20201221 E_Add
 
 $locs = $query->find_array();
 echo json_encode($locs);
