5a0f0cbb475e75364fd7f24b28b6e0a131239f63
tatsuya-maruyama <maruyama@wavesync.co.jp>
2021-07-03 05:36:54 +0900

    20210703_maruyama

--- api/getpaymenttype.php
+++ api/getpaymenttype.php
@@ -2,8 +2,18 @@
 require '../header.php';
 
 $postdata = file_get_contents("php://input");
+$param = json_decode($postparam);
 
-$paymenttypes = ORM::for_table(TBLPAYMENTTYPE)->where('payContractEntryFlg', '1')->where_null('deleteDate')->order_by_asc('displayOrder')->order_by_asc('paymentCode')->find_array();
+$query = ORM::for_table(TBLPAYMENTTYPE)->where_null('deleteDate');
+
+if(isset($param->paymentCode)){
+	$query = $query->where_in('paymentCode', $param->paymentCode);
+}
+if(isset($param->payContractEntryFlg)){
+	$query = $query->where_in('payContractEntryFlg', $param->payContractEntryFlg);
+}
+$paymenttypes = $query->order_by_asc('displayOrder')->order_by_asc('paymentCode')->find_array();
+
 echo json_encode($paymenttypes);
 
 ?>
