ac894ab01e5f03e259b4790e591307e6d8dcd18a
tatsuya-maruyama <maruyama@wavesync.co.jp>
2021-07-13 02:48:57 +0900

    20210713_maruyama

--- api/getdep.php
+++ api/getdep.php
@@ -3,7 +3,7 @@ require '../header.php';
 
 $postdata = file_get_contents("php://input");
 
-$deps = ORM::for_table(TBLDEPARTMENT)->where_null('deleteDate')->find_array();
+$deps = ORM::for_table(TBLDEPARTMENT)->where_null('deleteDate')->order_by_asc('displayOrder')->find_array();
 echo json_encode($deps);
 
 ?>
--- api/getemployee.php
+++ api/getemployee.php
@@ -4,13 +4,19 @@ require '../header.php';
 $postparam = file_get_contents("php://input");
 $param = json_decode($postparam);
 
-$query = ORM::for_table(TBLUSER)->where_null('deleteDate');
+//$query = ORM::for_table(TBLUSER)->where_null('deleteDate');
+$query = ORM::for_table(TBLUSER)
+			->table_alias('p1')
+			->select('p1.*')
+			->select('p2.displayOrder')
+			->left_outer_join(TBLDEPARTMENT, array('p1.depCode', '=', 'p2.depCode'), 'p2')
+			->where_null('p1.deleteDate');
 
 if(isset($param->activeUser) && $param->activeUser === '1'){
 	$query = $query->where_not_null('loginId')->where_not_equal('loginId','');
 }
 
-$emps = $query->where_not_equal('loginId', '0001')->where_not_equal('loginId', '0002')->order_by_asc('depCode')->order_by_asc('userId')->find_array();
+$emps = $query->where_not_equal('loginId', '0001')->where_not_equal('loginId', '0002')->order_by_asc('displayOrder')->order_by_asc('depCode')->order_by_asc('userId')->find_array();
 echo json_encode($emps);
 
 ?>
