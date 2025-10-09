d67f378cb7b996710c5f25a038e888857eb4147c
tatsuya-maruyama <maruyama@wavesync.co.jp>
2025-10-10 00:05:07 +0900

    20251009_maruyama

--- api/getemployee.php
+++ api/getemployee.php
@@ -21,7 +21,17 @@ if(isset($param->activeUser) && $param->activeUser === '1') {
 if(!isset($param->activeUser) || $param->activeUser !== '9') {
 	$query = $query->where_not_in('loginId', ['0001', '0002', '0003', '0004', '0005']);
 }
-$emps = $query->order_by_asc('displayOrder')->order_by_asc('depCode')->order_by_asc('userId')->find_array();
+
+if (is_array($param->depCode) && count($param->depCode) > 0) {
+	$query = $query->where_in('p1.depCode', $param->depCode);
+}
+// 20250924 S_Update
+$emps = $query->order_by_expr('CASE WHEN authority = 99 THEN 1 ELSE 0 END')
+	->order_by_expr('CASE WHEN userNameKana IS NULL THEN 1 ELSE 0 END')
+	->order_by_asc('userNameKana')
+	->order_by_asc('userId')
+	->find_array();
+// 20250924 E_Update
 // 20220517 E_Update
 
 echo json_encode($emps);
